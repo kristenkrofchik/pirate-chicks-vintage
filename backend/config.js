ddf69fc00815e853f462c0851b660963664ddbd3
Kristen Krofchik <k.krofchik@gmail.com>
2021-09-13 19:38:43 -0500

    backend config initial commit will need edits and personalization

---
+++ backend/config.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/** Config for application as a whole */
+
+require('dotenv').config();
+require('colors');
+
+const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
+
+const PORT = +process.env.PORT || 3001;
+
+/** Use either app database or testing database */
+function getDatabase() {
+    return(process.env.NODE_ENV === 'test')
+        ? 'pirate-chicks-test'
+        : process.env.DATABASE_URL || 'pirate-chicks';
+}
+
+/** Speed up bcrypt for testing */
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === 'test' ? 1 : 12;
+
+console.log('Pirate Chicks Config:'.green);
+console.log('SECRET_KEY:'.yellow, SECRET_KEY);
+console.log('PORT:'.yellow, PORT.toString());
+console.log('BCRYPT_WORK_FACTOR'.yellow, BCRYPT_WORK_FACTOR);
+console.log('Databse:'.yellow, getDatabase());
+console.log('---');
+
+module.exports = {
+    SECRET_KEY,
+    PORT,
+    BCRYPT_WORK_FACTOR,
+    getDatabase
+};
