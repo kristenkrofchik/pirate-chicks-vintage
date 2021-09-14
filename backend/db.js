bbdd3ac10f3463a37bf54e917c0ef052fc4a9c25
Kristen Krofchik <k.krofchik@gmail.com>
2021-09-13 19:44:54 -0500

    gneneral db setup

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** Database setup for pirate-chicks */
+const { Client } = require('pg');
+const { getDatabase } = require('./config');
+
+let db;
+
+if (process.env.NODE_ENV === 'production') {
+    db = new Client({
+        connectionString: getDatabase(),
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else {
+    db = new Client({
+        connectionString: getDatabase()
+    });
+}
+
+db.connect();
+
+module.exports = db;
