a2acac5624453e038164797b72b4c67d7e3b2c37
Kristen Krofchik <k.krofchik@gmail.com>
2021-09-23 15:37:12 -0500

    login form react

---
+++ frontend/src/auth/LoginForm.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Alert from "../common/Alert";
+
+/**Login form.
+ * Shows form, manages update to state on change.
+ * on submit- call login function prop, redirects to homepage /
+ * routed as /login
+ */
+
+function LoginForm({ login }) {
+    const history = useHistory();
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    });
+    const [formErrors, setFormErrors] = useState([]);
+
+    /**Handle form submit.
+     * calls login function prop, if successful redirects to homepage /
+     */
+
+    async function handleSubmit(evt) {
+        evt.preventDefault();
+        let result = await login(formData);
+        if(result.success) {
+            history.push('/');
+        } else {
+            setFormErrors(result.errors);
+        }
+    }
+
+    /**Handle changes to form data fields */
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+        setFormData(l => ({...l, [name]: value}));
+    }
+
+    return (
+        <div className="LoginForm">
+        <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+          <h3 className="mb-3">Log In</h3>
+
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label>Username</label>
+                  <input
+                      name="username"
+                      className="form-control"
+                      value={formData.username}
+                      onChange={handleChange}
+                      autoComplete="username"
+                      required
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input
+                      type="password"
+                      name="password"
+                      className="form-control"
+                      value={formData.password}
+                      onChange={handleChange}
+                      autoComplete="current-password"
+                      required
+                  />
+                </div>
+
+                {formErrors.length
+                    ? <Alert type="danger" messages={formErrors} />
+                    : null}
+
+                <button
+                    className="btn btn-primary float-right"
+                    onSubmit={handleSubmit}
+                >
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+
+}
+
+export default LoginForm;
